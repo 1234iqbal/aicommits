234168415fc00e852243cc6c3550bb8fd610751a
fatkhul jabal <mifjak1@gmail.com>
2023-03-24 11:08:08 +0700

    Update server.js with new message to send in response to GET request

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Belajar Commit With Ai!')
 })
     
 app.listen(port, () => console.log(`Hello Node app listening on port ${port}!`) );
+
+
+
+
+
+
+
+
